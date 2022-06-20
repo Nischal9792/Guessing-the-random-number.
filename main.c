c39dc643068f6bab09c48e9a8323b418480c85bf
Nischal9792 <100753649+Nischal9792@users.noreply.github.com>
2022-06-20 11:45:40 +0545

    Create main.c

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+int main()
+{
+    int number, guess, nguesses=1;
+    srand(time(0));
+    number = rand()%100 +1;
+    // printf("The number is %d\n", number);
+
+    do{
+        printf("Guess the number:\n");
+        scanf("%d", &guess);
+        if(guess>number){
+            printf("Lower number please\n");
+        }
+        else if(guess<number){
+            printf("Higher number please\n");
+        }
+        else{
+            printf("You guessed it in %d attempts\n", nguesses);
+        }
+        nguesses++;
+    }while(guess!=number);
+    return 0;
+}
